21ec21c4b7b76d45c7a8d3d1722cedd2299d1abb
Rafhael Barabas <rafhael.barabas@totvs.com.br>
2022-06-29 15:14:35 -0300

    Atualiza readme e cria uma action para automatizar alguns processos

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+const getAge = () => {
+  const birthDate = new Date(1994, 0, 28);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  return today.getMonth() >= birthDate.getMonth() &&
+    today.getDate() >= birthDate.getDate()
+    ? age
+    : --age;
+};
+
+fs.readFile("README_base.md", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const replaceVars = {
+    age: getAge(),
+    company: "TOVTVS Santa Catarina", // TODO: Automatizar com a LinkeIn API
+  };
+
+  const updatedReadMe = data.replace(
+    /%{.*}/gm,
+    (e) => replaceVars?.[e.slice(2, -1)] || e
+  );
+
+  fs.writeFile("README.md", updatedReadMe, "utf-8", (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("✔ Finished!");
+  });
+});
